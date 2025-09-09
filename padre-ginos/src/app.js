ef9767fca6774ffde37a9d7489b955c78189e322
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-09-09 03:21:18 -0700

    Add Pizza component to app.js
    Introduced a new Pizza component that displays a pizza name and its ingredients. No changes were made to the main App component's rendered output.

--- padre-ginos/src/app.js
+++ padre-ginos/src/app.js
@@ -1,11 +1,18 @@
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "padre Gino's"),
-    );
-}
+const Pizza = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "The Selam Pizza"),
+    React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),
+  ]);
+};
 
-const container = document.getElementById("root")
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "padre Gino's")
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
